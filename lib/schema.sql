6051c0120ca76f7f314917c2b0c882b6bdddb49d
Cody Frank <cody.frank30@gmail.com>
2019-06-23 23:16:06 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Series(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
+CREATE TABLE SubGenres(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE Authors(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE Books(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  series_id INTEGER
+);
+
+CREATE TABLE Characters(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
